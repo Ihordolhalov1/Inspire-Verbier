5944851650de9a131e271d2ebfe420ce011618e8
Ihordolhalov1 <servicegambro@gmail.com>
2022-10-11 18:18:12 +0100

    deleted cookies request if user doesn't allow tracking add activityIndicator when loading cart.mindbodyonline.com

--- InspireVerbier/SecondViewController.swift
+++ InspireVerbier/SecondViewController.swift
@@ -21,51 +21,34 @@ class SecondViewController: UIViewController, WKNavigationDelegate, WKUIDelegate
     
     @IBOutlet weak var forwardButton: UIButton!
     
+    // инициализируем activityIndicator чтоб он появился после кнопки book на время загрузки следующей страницы
+    lazy var activityIndicator: UIActivityIndicatorView = {
+    let indicator = UIActivityIndicatorView(style: .medium)
+    indicator.frame = view.frame
+    indicator.backgroundColor = .systemGray6
+    return indicator
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         webView.uiDelegate = self
-        
         webView.navigationDelegate = self
         backButton.isEnabled = false
         forwardButton.isEnabled = false
         
         let url = URL(string: "https://www.inspireverbier.com/timetable")
-        //let url = URL(string: "https://www.apple.com")
-        var request = URLRequest(url: url!)
-        //webView.configuration.preferences.javaScriptEnabled = true
+        let request = URLRequest(url: url!)
+        // webView.configuration.preferences.javaScriptEnabled = true
         webView.configuration.defaultWebpagePreferences.allowsContentJavaScript = true
         webView.allowsBackForwardNavigationGestures = true
-        
-        // Знову перевіряємо чи маємо дозвіл на трекінг, якщо ні - видалити кукіс
-        guard #available(iOS 14, *) else { return }
-        ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-                  DispatchQueue.main.async {
-                      ATTrackingManager.requestTrackingAuthorization {status in
-                          switch status {
-                              // Проверяем status
-                          case .notDetermined:
-                              print ("notDetermined")
-                          case .restricted:
-                              print ("restricted")
-                          case .denied:
-                              print ("denied")
-                              //видалити кукіс і ніколи іх не збирати
-                              request.httpShouldHandleCookies = false
-                              HTTPCookieStorage.shared.cookieAcceptPolicy = .never
-                          case .authorized:
-                              print ("authorized")
-                          @unknown default:
-                              print ("unknown")
-                          }
-                      }
-                  }
-        })
-
+  
         // Загружаємо склад сторінки
+     
         webView.load(request)
+        
+        view.addSubview(activityIndicator)
+        
         if webView.canGoBack == false {
             backButton.isEnabled = false} else { backButton.isEnabled = true }
         if webView.canGoForward == false {
@@ -118,10 +101,18 @@ class SecondViewController: UIViewController, WKNavigationDelegate, WKUIDelegate
         return
         }
         
+     /*   if #available(iOS 16.0, *) {
+            if url.host() == "cart.mindbodyonline.com" { // запустить индикатор при загрузке страницы
+                activityIndicator.startAnimating() }
+           
+        } else {   */
+               if url.host == "cart.mindbodyonline.com" { // запустить индикатор при загрузке страницы
+               activityIndicator.startAnimating()}
+        
+        
         decisionHandler(.allow)
     }
 
-    
    
     func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
         if navigationAction.targetFrame == nil {
@@ -145,6 +136,39 @@ class SecondViewController: UIViewController, WKNavigationDelegate, WKUIDelegate
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+              // Знову перевіряємо чи маємо дозвіл на трекінг, якщо ні - видалити кукіс
+        guard #available(iOS 14, *) else { return }
+        ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                  DispatchQueue.main.async {
+                      ATTrackingManager.requestTrackingAuthorization {status in
+                          switch status {
+                              // Проверяем status
+                          case .notDetermined:
+                              print ("notDetermined")
+                          case .restricted:
+                              print ("restricted")
+                          case .denied:
+                              print ("denied")
+                              //видалити кукіс і ніколи іх не збирати
+                              let javascript = "function waitForElm(e){return new Promise(r=>{if(document.querySelector(e))return r(document.querySelector(e));let o=new MutationObserver(t=>{document.querySelector(e)&&(r(document.querySelector(e)),o.disconnect())});o.observe(document.body,{childList:!0,subtree:!0})})}waitForElm('div[aria-labelledby=cookies_policy_description]').then(e=>{e.remove()});"
+                              webView.evaluateJavaScript(javascript) { (result, error) in
+                                   print("Код выполнился!")
+                              }
+                                //request.httpShouldHandleCookies = false
+                              HTTPCookieStorage.shared.cookieAcceptPolicy = .never
+                          case .authorized:
+                              print ("authorized")
+                          @unknown default:
+                              print ("unknown")
+                          }
+                      }
+                  }
+        })
+
+        if activityIndicator.isAnimating { //остановить activityIndicator
+             activityIndicator.stopAnimating()
+        }
+       
        backButton.isEnabled = webView.canGoBack
       forwardButton.isEnabled = webView.canGoForward
           }
